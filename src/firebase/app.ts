8edfa157d24d7008b3e85b05532df0fe347d792a
Dmytro Borchuk <frunk.lern@gmail.com>
2022-11-01 22:41:44 +0200

    Added firebase

---
+++ src/firebase/app.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}.firebaseapp.com`,
+  databaseURL: `https://${process.env.REACT_APP_FIREBASE_PROJECT_ID}.firebaseio.com`,
+  projectId: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}.appspot.com`,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+}
+
+export const app = initializeApp(firebaseConfig)
+
+if (process.env.NODE_ENV === 'development') {
+  const analytics = getAnalytics(app)
+}
